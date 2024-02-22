96a5aa9a2a5f16eae91a0d995a116a05682d295b
GGnagaraxx <giodelacruz05@gmail.com>
2024-02-22 11:53:42 +0800

    Deployment Setup Correction

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/XstaticGames/",
+  base: "/XtaticGames/",
   plugins: [react()],
 })
